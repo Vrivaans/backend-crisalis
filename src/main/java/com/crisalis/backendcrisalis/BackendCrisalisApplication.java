fe0079a16d04bfa7a9d01eeaddaab662e2ba4aff
Vrivaans <vrivaans@gmail.com>
2022-12-02 10:00:20 -0300

    primer commit

---
+++ src/main/java/com/crisalis/backendcrisalis/BackendCrisalisApplication.java
@@ -0,0 +1,13 @@
+package com.crisalis.backendcrisalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrisalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrisalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crisalis/backendcrisalis/BackendCrisalisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisalis.backendcrisalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrisalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
