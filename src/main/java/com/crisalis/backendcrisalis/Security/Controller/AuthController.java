0ca6ebc567c9c52de5d62c455dd1a6632bde312a
Vrivaans <vrivaans@gmail.com>
2022-12-12 10:15:11 -0300

    Cors configurado y usuario agregado

--- src/main/java/com/crisalis/backendcrisalis/Security/Controller/AuthController.java
+++ src/main/java/com/crisalis/backendcrisalis/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost")
 
 public class AuthController {
     @Autowired
